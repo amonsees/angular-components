c742103d6ac92d5e282d8636908812d570a28051
Anthony Monsees <anthony.monsees@devwareinc.com>
2019-09-11 09:50:49 -0500

    Small update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
-
+ 
 @NgModule({
   declarations: [
     AppComponent,
